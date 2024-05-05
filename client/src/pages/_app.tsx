70b9ea75a375751111ed10ada88786897fde5be9
Emil Djupvik <emil@djupvik.dev>
2024-05-05 16:52:12 +0200

    add noStore to include environment variables client side

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { unstable_noStore as noStore } from 'next/cache'
 import Head from 'next/head';
 import { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -5,7 +6,8 @@ import { Global } from '@emotion/react';
 
 
 export default function home({ Component, pageProps }: AppProps) {
-    const hasClerkVariables = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+    noStore();
+    const hasClerkVariables = !!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
     return (<>
         <Global styles={"body {margin: 0;}"} />
