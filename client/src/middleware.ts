fa4913e53f9d8e227bcb5401070d2ef47b5032d0
Emil Djupvik <emil@djupvik.dev>
2024-07-01 00:47:13 +0200

    implement / as a protected route

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware((auth, req) => {
+    if (!auth().userId && isProtectedRoute(req)) {
+        return auth().redirectToSignIn();
+    }
+});
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next|api/mqtt).*)", "/"],
