ca678a0f996b47c73d19edc3805408ff542cafbe
Emil Djupvik <emil@djupvik.dev>
2024-10-13 15:35:53 +0200

    delete old stuff

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,7 +3,6 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/"]);
 
 export default clerkMiddleware((auth, req) => {
-    auth().getToken().then(console.log)
     if (!auth().userId && isProtectedRoute(req)) {
         return auth().redirectToSignIn();
     }
