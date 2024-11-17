1247b48d84108f848df3e04d6a8d3a05264ea062
Emil Djupvik <emil@djupvik.dev>
2024-11-17 20:40:32 +0100

    get available ledstrips from database

--- client/src/app/[id]/page.tsx
+++ client/src/app/[id]/page.tsx
@@ -1,34 +1,10 @@
 import React from "react";
-import { auth } from "@clerk/nextjs/server";
-import { fetchSavedStates } from "@/services/users";
 import { ModesProvider } from "@/contexts/ModesContext";
 import ModesComponent from "@/components/ModesComponent";
 
-
-async function fetchData() {
-    const { userId } = auth();
-
-
-    if (!userId) {
-        return {
-            redirect: {
-                destination: "/sign-in",
-                permanent: false,
-            },
-        };
-    }
-
-    return {
-        initialSavedStates: await fetchSavedStates(userId),
-    };
-}
-
-
-
 export default async function Page(context: any) {
-    const { initialSavedStates } = await fetchData();
     return <ModesProvider id={context.params.id}>
-        <ModesComponent initialSavedStates={initialSavedStates!} />
+        <ModesComponent />
     </ModesProvider>
 
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,23 @@
+import { AppBarComponent } from "@/components/AppBarComponent";
 import { LedStripsComponent } from "@/components/LedStripsComponent";
 import { LedStripProvider } from "@/contexts/LedStripsContext";
+import { auth } from "@clerk/nextjs/server";
+import { PrismaClient } from "@prisma/client";
 
-export default function Page() {
-    return <LedStripProvider>
+const prisma = new PrismaClient();
+
+export default async function Page() {
+    const { userId } = auth();
+
+    const user = await prisma.user.findUnique({
+        where: { id: userId! },
+        include: { ledstrips: true },
+    });
+
+    // TODO: handle uninitialized user
+
+    return <LedStripProvider ledstrips={user!.ledstrips!}>
+        <AppBarComponent />
         <LedStripsComponent />
     </LedStripProvider>
 }
