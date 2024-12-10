53ca394188de81df7a25b55553cc3ba1656b4077
Emil Djupvik <emil@djupvik.dev>
2024-12-10 19:18:54 +0100

    prettify access tokens component

--- client/src/components/AccessTokensComponent.tsx
+++ client/src/components/AccessTokensComponent.tsx
@@ -1,30 +1,49 @@
 import { useAccessTokens, useCreateAccessToken, useDeleteAccessToken } from '@/contexts/AccessTokensContext';
-import { Button, List, ListItem, ListItemText, IconButton } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Button, List, ListItem, ListItemText, IconButton, Divider } from '@mui/material';
+import { Delete, ContentCopy } from '@mui/icons-material';
 import React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 
 export const AccessTokensComponent: React.FC = () => {
     const tokens = useAccessTokens();
     const addToken = useCreateAccessToken();
     const deleteToken = useDeleteAccessToken();
 
-    console.log(tokens);
+    async function handleCopy(token: string) {
+        navigator.clipboard.writeText(token)
+            .then(() => {
+                toast.success('Token copied to clipboard');
+            })
+            .catch(() => {
+                toast.error('Failed to copy token to clipboard');
+            });
+    }
 
     return (
-        <div>
+        <>
             <List>
                 {tokens.map((token, index) => (
-                    <ListItem key={index}>
-                        <ListItemText primary={token.token} />
-                        <IconButton edge="end" aria-label="delete" onClick={() => deleteToken(token.id)}>
-                            <DeleteIcon />
-                        </IconButton>
-                    </ListItem>
+                    <>
+                        <ListItem key={index}>
+                            <IconButton edge="end" aria-label="copy" onClick={() => handleCopy(token.token)} sx={{
+                                margin: 0,
+                            }}>
+                                <ContentCopy />
+                            </IconButton>
+                            <ListItemText primary={token.token} />
+                            <IconButton edge="end" aria-label="delete" onClick={() => deleteToken(token.id)}>
+                                <Delete />
+                            </IconButton>
+                        </ListItem>
+                        <Divider />
+                    </>
+
                 ))}
             </List>
             <Button variant="contained" color="primary" onClick={addToken}>
                 Add Access Token
             </Button>
-        </div>
+            <ToastContainer />
+        </>
     );
 };
